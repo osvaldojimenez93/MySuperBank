9cb0eee71445ef1e99c5c119589217a572d20330
Osvaldo Jimenez <osjimene@microsoft.com>
2020-04-27 15:26:09 -0700

    Add project files.

---
+++ MySuperBank/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Jimbo", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with ${account.Balance}.");
+
+            account.MakeWithdrawl(120, DateTime.Now, "Hammock");       
+            account.MakeWithdrawl(50, DateTime.Now, "Xbox Game");
+           
+
+            Console.WriteLine(account.GetAccountHistory());
+
+
+
+
+
+
+
+
+            //// Test that the initial balances must be positive.
+            //try
+            //{
+            //    var invalidAccount = new BankAccount("invalid", -55);
+            //}
+            //catch (ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("Exception caught creating account with negative balance");
+            //    Console.WriteLine(e.ToString());
+            //}
+
+
+        }
+
+    }
+}
